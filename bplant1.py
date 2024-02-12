68b0d8c0c60f568ab05723992ba440edd5b906af
Chris Warren <cwarren@vidmob.com>
2024-02-11 20:11:23 -0500

    change plant growth functions to paramaterize things that were previously references to global constants

--- bplant1.py
+++ bplant1.py
@@ -82,21 +82,16 @@ MAX_PARTICLE_INJECT_INNER_RADIUS = max(IMAGE_WIDTH, IMAGE_HEIGHT) // 4 # inner r
 
 SEED_RADIUS = 4 
 
-
-##################################
-# FUNCTIONS - general planar stuff
-
-
-
 ##################################
 # FUNCTIONS - plant growth stuff
 
-def is_adjacent_to_live_pixel(point):
+def is_adjacent_to_live_pixel(point, pixels, dead_colors):
     """
     Determine if the given point is adjacent to a live pixel, where 'live' is defined as a pixel that has a color value that's not in the DEAD_COLORS list
 
     Parameters:
-    - point: an (x,y) tuple, using an image orientation of the plane (i.e. upper left is 0,0
+    - point: an (x,y) tuple, using an image orientation of the plane (i.e. upper left is 0,0)
+    - pxs: a grid of pixel values, using an image orientation of the plane (i.e. upper left is 0,0)
 
     Returns:
     - True if the point is adjacent to a live pixel, False otherwise
@@ -106,7 +101,7 @@ def is_adjacent_to_live_pixel(point):
         try:
             # Check if the adjacent point is within the image bounds
             # NOTE: color check uses [:3] since the source has an alpha channel that we don't care about
-            if PIXELS[adj_point][:3] not in DEAD_COLORS:
+            if pixels[adj_point][:3] not in dead_colors:
                 return True
         except IndexError:
             # If adj_point is out of image bounds, ignore it
@@ -132,7 +127,7 @@ def move_particle(point, strategy = 'FULL_RANDOM_DRIFT'):
         
     return pu.constrain_point_to_bounding_box(point,IMAGE_BOUNDING_BOX)
 
-def grow_at(point, strategy = 'DEPOSIT'):
+def grow_at(point, pixels, strategy = 'DEPOSIT'):
     """
     grow a plant at the given point according to the given strategy.
 
@@ -145,23 +140,31 @@ def grow_at(point, strategy = 'DEPOSIT'):
     - None
     """
     if strategy == 'DEPOSIT':
-        PIXELS[point[0],point[1]] = COLOR_PLANT
+        pixels[point[0],point[1]] = COLOR_PLANT
 
-def set_up_plant_seed():
+def set_up_plant_seed(image, seed_radius, fill_color):
     """
     Set up the plant seed.
 
     Returns:
     - an (x,y) tuple representing the center of the plant seed
     """
-    seedx, seedy = IMAGE_WIDTH // 2, IMAGE_HEIGHT - 1
-    seed_left_up_point = (seedx - SEED_RADIUS, seedy - SEED_RADIUS)
-    seed_right_down_point = (seedx + SEED_RADIUS, seedy + SEED_RADIUS)
-    DRAW.ellipse([seed_left_up_point, seed_right_down_point], outline=COLOR_PLANT, fill=COLOR_PLANT)
+    # seedx, seedy = IMAGE_WIDTH // 2, IMAGE_HEIGHT - 1
+    # seed_left_up_point = (seedx - SEED_RADIUS, seedy - SEED_RADIUS)
+    # seed_right_down_point = (seedx + SEED_RADIUS, seedy + SEED_RADIUS)
+    # DRAW.ellipse([seed_left_up_point, seed_right_down_point], outline=COLOR_PLANT, fill=COLOR_PLANT)
+    # seed_center = (seedx, seedy)
+    # return seed_center
+    im_w, im_h = image.size
+    draw = ImageDraw.Draw(image)
+    seedx, seedy = im_w // 2, im_h - 1
+    seed_left_up_point = (seedx - seed_radius, seedy - seed_radius)
+    seed_right_down_point = (seedx + seed_radius, seedy + seed_radius)
+    draw.ellipse([seed_left_up_point, seed_right_down_point], outline=fill_color, fill=fill_color)
     seed_center = (seedx, seedy)
     return seed_center
 
-def get_particle_action_radii_from_base_radius(base_radius):
+def get_particle_action_radii_from_base_radius(base_radius, inner_radius_factor, outer_radius_factor, movement_radius_extension, max_inner_radius):
     """
     Get a list of particle radii, given the base radius.
 
@@ -171,12 +174,15 @@ def get_particle_action_radii_from_base_radius(base_radius):
     Returns:
     - a list of particle radii: inject_inner_radius, inject_outer_radius, and max_movement_radius
     """
-    particle_inject_inner_radius = min(MAX_PARTICLE_INJECT_INNER_RADIUS, base_radius * PARTICLE_INJECTION_MIN_RADIUS_FACTOR)
-    particle_inject_outer_radius = base_radius * PARTICLE_INJECTION_MAX_RADIUS_FACTOR
-    particle_max_movement_radius = particle_inject_outer_radius + PARTICLE_MOVEMENT_MAX_RADIUS_EXTENSION
+    # particle_inject_inner_radius = min(MAX_PARTICLE_INJECT_INNER_RADIUS, base_radius * PARTICLE_INJECTION_MIN_RADIUS_FACTOR)
+    # particle_inject_outer_radius = base_radius * PARTICLE_INJECTION_MAX_RADIUS_FACTOR
+    # particle_max_movement_radius = particle_inject_outer_radius + PARTICLE_MOVEMENT_MAX_RADIUS_EXTENSION
+    particle_inject_inner_radius = min(max_inner_radius, base_radius * inner_radius_factor)
+    particle_inject_outer_radius = base_radius * outer_radius_factor
+    particle_max_movement_radius = particle_inject_outer_radius + movement_radius_extension
     return particle_inject_inner_radius, particle_inject_outer_radius, particle_max_movement_radius
 
-def injected_particle(inject_center, inner_radius, outer_radius):
+def injected_particle(inject_center, inner_radius, outer_radius, image_bounds):
     """
     Get an (x,y) tuple representing a particle that has been injected into the growing medium
 
@@ -189,7 +195,7 @@ def injected_particle(inject_center, inner_radius, outer_radius):
     - an (x,y) tuple representing the center of the injected particle, where x and y are integers
     """
     p = pu.get_random_point_in_ring(inject_center, inner_radius, outer_radius)
-    while not pu.is_point_in_rect(p, IMAGE_BOUNDING_BOX):
+    while not pu.is_point_in_rect(p, image_bounds):
         p = pu.get_random_point_in_ring(inject_center, inner_radius, outer_radius)
     return p
 
@@ -216,15 +222,21 @@ def lpad(tnum, n):
 # MAIN
 
 def main():
-    particle_inject_center = set_up_plant_seed()
+    particle_inject_center = set_up_plant_seed(IMAGE, SEED_RADIUS, COLOR_PLANT)
 
-    particle_inject_inner_radius, particle_inject_outer_radius, particle_max_movement_radius = get_particle_action_radii_from_base_radius(SEED_RADIUS)
+    particle_inject_inner_radius, particle_inject_outer_radius, particle_max_movement_radius = get_particle_action_radii_from_base_radius(
+        SEED_RADIUS,
+        PARTICLE_INJECTION_MIN_RADIUS_FACTOR,
+        PARTICLE_INJECTION_MAX_RADIUS_FACTOR,
+        PARTICLE_MOVEMENT_MAX_RADIUS_EXTENSION,
+        MAX_PARTICLE_INJECT_INNER_RADIUS
+        )
 
     tmark_first, tmark_last, tmark_cur = time.time(), time.time(), time.time()
 
     particles = []
     for i in range(PARTICLE_COUNT):
-        particles.append(injected_particle(particle_inject_center, particle_inject_inner_radius, particle_inject_outer_radius))
+        particles.append(injected_particle(particle_inject_center, particle_inject_inner_radius, particle_inject_outer_radius, IMAGE_BOUNDING_BOX))
     debug(f"{len(particles)} particles injected")
     debug(f"particles: {particles}")
 
@@ -248,14 +260,21 @@ def main():
 
         particle = move_particle(particle)
 
-        if is_adjacent_to_live_pixel(particle):
-            grow_at(particle)
+        if is_adjacent_to_live_pixel(particle, PIXELS, DEAD_COLORS):
+            grow_at(particle, PIXELS)
             growth_counter += 1
             growth_radius = pu.distance_between(particle_inject_center,particle)
             if growth_radius > plant_radius:
                 plant_radius = growth_radius
-                particle_inject_inner_radius, particle_inject_outer_radius, particle_max_movement_radius = get_particle_action_radii_from_base_radius(plant_radius)
-            new_particle = injected_particle(particle_inject_center, particle_inject_inner_radius, particle_inject_outer_radius)
+                particle_inject_inner_radius, particle_inject_outer_radius, particle_max_movement_radius = get_particle_action_radii_from_base_radius(
+                    plant_radius,
+                    PARTICLE_INJECTION_MIN_RADIUS_FACTOR,
+                    PARTICLE_INJECTION_MAX_RADIUS_FACTOR,
+                    PARTICLE_MOVEMENT_MAX_RADIUS_EXTENSION,
+                    MAX_PARTICLE_INJECT_INNER_RADIUS
+                    )
+
+            new_particle = injected_particle(particle_inject_center, particle_inject_inner_radius, particle_inject_outer_radius, IMAGE_BOUNDING_BOX)
             particles.append(new_particle)
             if DO_PROGRESS_LOGGING and growth_counter % PROGRESS_LOGGING_INTERVAL == 0:
                 tmark_cur = time.time()
@@ -271,7 +290,7 @@ def main():
         else:
             particle_distance = pu.distance_between(particle_inject_center,particle)
             if particle_distance > particle_max_movement_radius:
-                particle = injected_particle(particle_inject_center, particle_inject_inner_radius, particle_inject_outer_radius)
+                particle = injected_particle(particle_inject_center, particle_inject_inner_radius, particle_inject_outer_radius, IMAGE_BOUNDING_BOX)
             particles.append(particle)
             if DO_PARTICLE_TRACING:
                 PIXELS[particle[0],particle[1]] = COLOR_PARTICLE_CUR
