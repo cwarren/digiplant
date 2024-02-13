b72d1bd97e5ea5424f814e3cfed51426865d12a1
Chris Warren <cwarren@vidmob.com>
2024-02-13 16:45:54 -0500

    grow plant using info from plant_genetics.yaml

--- bplant1.py
+++ bplant1.py
@@ -21,8 +21,8 @@ import yaml
 
 DEBUG_LOW = 1
 DEBUG_DEVELOPING = 2
-DEBUG_RICH = 2
-DEBUG_VERY_RICH = 2
+DEBUG_RICH = 3
+DEBUG_VERY_RICH = 4
 DEBUG_EXTREME = 5
 
 USING_DEBUG_LEVEL = DEBUG_DEVELOPING
@@ -87,7 +87,7 @@ MAX_PARTICLE_INJECT_INNER_RADIUS = int(max(IMAGE_WIDTH, IMAGE_HEIGHT) * .8) # in
 ##################################
 # PLANT DATA
 
-SEED_RADIUS = 4 
+# SEED_RADIUS = 4 
 
 ##################################
 
@@ -101,9 +101,16 @@ def setup_derived_plant_genetics(plant_genetics):
     Returns:
     - None
     """
+    plant_genetics['color_rgb_bg'] = tuple(plant_genetics['color_rgb_bg'])
+    plant_genetics['color_rgba_bg'] = tuple(plant_genetics['color_rgba_bg'])
+    plant_genetics['color_rgb_plant'] = tuple(plant_genetics['color_rgb_plant'])
+
     plant_genetics['dead_colors'] = [plant_genetics['color_rgb_bg'], COLOR_RGB_PARTICLE_TRACE, COLOR_RGB_PARTICLE_CUR]
     plant_genetics['max_particle_inject_inner_radius'] = int(max(plant_genetics['width'], plant_genetics['height']) * .8) # inner radius for injection can go most of the way to the edge
-
+    if plant_genetics['seed_location'] == 'BOTTOM_CENTER':
+        plant_genetics['particle_inject_center'] = (plant_genetics['width'] // 2, plant_genetics['height'] - 1)
+    else: # default is bottom center
+        plant_genetics['particle_inject_center'] = (plant_genetics['width'] // 2, plant_genetics['height'] - 1)
 
 def lpad(tnum, n):
     """
@@ -192,33 +199,34 @@ def grow_radii(particle_that_grew, plant_center, plant_radius, particle_inject_i
 ##################################
 # MAIN
 
-def main():
-    particle_inject_center = pg.setup_plant_seed_bottom_center(IMAGE, SEED_RADIUS, COLOR_RGB_PLANT)
+def main(plant_genetics):
+
+    pg.setup_plant_seed_bottom_center(IMAGE,plant_genetics['seed_radius'], plant_genetics['color_rgb_plant'])    
 
     particle_inject_inner_radius, particle_inject_outer_radius, particle_max_movement_radius = pg.get_particle_action_radii_from_base_radius(
-        SEED_RADIUS,
-        PARTICLE_INJECTION_MIN_RADIUS_FACTOR,
-        PARTICLE_INJECTION_MAX_RADIUS_FACTOR,
-        PARTICLE_MOVEMENT_MAX_RADIUS_EXTENSION,
-        MAX_PARTICLE_INJECT_INNER_RADIUS
+        plant_genetics['seed_radius'],
+        plant_genetics['particle_injection_min_radius_factor'],
+        plant_genetics['particle_injection_max_radius_factor'],
+        plant_genetics['particle_movement_max_radius_extension'],
+        plant_genetics['max_particle_inject_inner_radius']
         )
 
     tmark_first, tmark_last = time.time(), time.time()
 
-    particles = pg.setup_particle_list(PARTICLE_COUNT, particle_inject_center, particle_inject_inner_radius, particle_inject_outer_radius, IMAGE_BOUNDING_BOX)
+    particles = pg.setup_particle_list(plant_genetics['particle_count'], plant_genetics['particle_inject_center'], particle_inject_inner_radius, particle_inject_outer_radius, IMAGE_BOUNDING_BOX)
     debug(f"{len(particles)} particles injected")
     debug(f"particles: {particles}", DEBUG_DEVELOPING)
 
     # create incremental output file base name, based on growth size and timestamp
-    incremental_output_file_base = f"plant_{GROW_AMOUNT}_{tmark_first}_incr"
+    incremental_output_file_base = f"plant_{plant_genetics['grow_amount']}_{tmark_first}_incr"
     debug(f"incremental_output_file_base: {incremental_output_file_base}", DEBUG_DEVELOPING)
 
     # MAIN LOOP
     ## while the plant is growing, get a particle, move it, and append it back on the list; handle growth and out-of-bounds replacement as needed
     growth_counter = 0
     incremental_output_counter = 0
-    plant_radius = SEED_RADIUS
-    while growth_counter < GROW_AMOUNT:
+    plant_radius = plant_genetics['seed_radius']
+    while growth_counter < plant_genetics['grow_amount']:
         particle = particles.pop(0)
         debug(f"acting on particle {particle}", DEBUG_EXTREME)
 
@@ -227,25 +235,25 @@ def main():
 
         particle = pg.move_particle(particle, IMAGE_BOUNDING_BOX)
 
-        if pg.is_adjacent_to_live_pixel(particle, PIXELS, DEAD_COLORS, IMAGE_BOUNDING_BOX):
+        if pg.is_adjacent_to_live_pixel(particle, PIXELS, plant_genetics['dead_colors'], IMAGE_BOUNDING_BOX):
             growth_counter += 1
-            pg.grow_at(particle, PIXELS, COLOR_RGB_PLANT)
+            pg.grow_at(particle, PIXELS, plant_genetics['color_rgb_plant'])
             debug(f"grew at {particle}", DEBUG_VERY_RICH)
 
             plant_radius, particle_inject_inner_radius, particle_inject_outer_radius, particle_max_movement_radius = grow_radii(particle, 
-                                                                                                                                particle_inject_center, 
+                                                                                                                                plant_genetics['particle_inject_center'], 
                                                                                                                                 plant_radius, 
                                                                                                                                 particle_inject_inner_radius, 
                                                                                                                                 particle_inject_outer_radius, 
                                                                                                                                 particle_max_movement_radius)
 
-            new_particle = pg.injected_particle_ring(particle_inject_center, particle_inject_inner_radius, particle_inject_outer_radius, IMAGE_BOUNDING_BOX)
+            new_particle = pg.injected_particle_ring(plant_genetics['particle_inject_center'], particle_inject_inner_radius, particle_inject_outer_radius, IMAGE_BOUNDING_BOX)
             particles.append(new_particle)
             tmark_last = handle_progress_logging(growth_counter, tmark_last)
             incremental_output_counter = handle_incremental_output(incremental_output_counter, growth_counter, incremental_output_file_base)
         else:
             particle = pg.get_particle_within_movement_bounds_ring(particle,
-                                                                   particle_inject_center, 
+                                                                   plant_genetics['particle_inject_center'], 
                                                                    particle_inject_inner_radius, 
                                                                    particle_inject_outer_radius, 
                                                                    particle_max_movement_radius, 
@@ -255,10 +263,11 @@ def main():
                 PIXELS[particle[0],particle[1]] = COLOR_RGB_PARTICLE_CUR
 
     total_elapsed_s = int((time.time() - tmark_first))
-    final_output_path = f"greenhouse/plant_{GROW_AMOUNT}_{tmark_first}_{total_elapsed_s}.png"
+    final_output_path = f"greenhouse/plant_{plant_genetics['grow_amount']}_{tmark_first}_{total_elapsed_s}.png"
     IMAGE.save(final_output_path)
     print(f"Done. Total elapsed time for plant generation: {total_elapsed_s} s")
     print(f"Image saved to {final_output_path}")
+
 
 if __name__ == "__main__":
     with open("plant_genetics.yaml", 'r') as stream:
@@ -266,7 +275,7 @@ if __name__ == "__main__":
     setup_derived_plant_genetics(plant_genetics)
 
     debug(f"plant_genetics: {plant_genetics}", DEBUG_DEVELOPING)
-    sys.exit()
+    # sys.exit()
 
 
-    main()
+    main(plant_genetics)
